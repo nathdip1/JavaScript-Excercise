80c8966522200b00d8598b48a77854b8569c6377
Dipjyoti <nathdipjyoti844@gmail.com>
2024-12-27 23:51:27 +0530

    Prime Number commit

--- primeNumber.js
+++ primeNumber.js
@@ -1,5 +1,5 @@
 
-
+// Using array
 function prime(n){
     for (let i=1;i<=n;i++){
         if(n%i === 0)
@@ -17,5 +17,49 @@ function prime(n){
     let num = 3;
     let count = [];
     prime(num);
+
+// Using function of normal logic
+
+    // function primeNumber(num){
+    //     let count = 0, tempNum;
+    //     if(num===0||num===1)
+    //         console.log("Test a number GT 1");
+    //     else{
+    //         for(let i=2; i<=num; i++){
+    //             tempNum = num%i;
+    //             if(tempNum===0)
+    //                 count++;
+    //             }
+    //             if(count===1)
+    //                 console.log("The given number is a Prime Number");
+    //             else
+    //                 console.log("The given number isn't a Prime Number");
+    //         }
+                
+    // }
     
+    // const num = 7;
+    // primeNumber(num);
     
+    // using Math.sqrt() function
+
+    // function primeNumber(num){
+    //     if(num===0||num===1)
+    //         console.log("Test a number GT 1");
+    //     else{
+    //         for(let i=2; i<=Math.sqrt(num); i++){
+    //             if(num%i===0)
+    //                 return false;
+    //             else
+    //                 return true;
+                    
+    //             }
+    //         }
+                
+    // }
+    
+    // const num = 8;
+    // console.log(primeNumber(num));
+
+
+
